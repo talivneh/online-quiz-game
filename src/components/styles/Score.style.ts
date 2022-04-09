8a0fd63ed9c948d798e1a373b730a278a787be0c
Tal Livneh <50097959+talivneh@users.noreply.github.com>
2022-04-09 16:10:35 +0300

    last commit

--- src/components/styles/Score.style.ts
+++ src/components/styles/Score.style.ts
@@ -4,7 +4,7 @@ export const StyledScoreWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-around;
-  width: 130px;
+  min-width: 130px;
   svg {
     color: #ff7f50;
   }
